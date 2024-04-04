88e0d94b97aa019958d7fa7d28320c0994b93c4d
Alexx mzz <dani315788@gmail.com>
2024-04-04 13:45:04 -0500

    Uso de importar un excel, leer datos, mostrarlos en un formulario y exportar los datos a un excel

---
+++ ImportaExportaEXCEL/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace ImportaExportaEXCEL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnImportar = new System.Windows.Forms.Button();
+            this.btnExportar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.btnExportar);
+            this.groupBox1.Controls.Add(this.btnImportar);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(493, 356);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnImportar
+            // 
+            this.btnImportar.Location = new System.Drawing.Point(78, 52);
+            this.btnImportar.Name = "btnImportar";
+            this.btnImportar.Size = new System.Drawing.Size(95, 34);
+            this.btnImportar.TabIndex = 0;
+            this.btnImportar.Text = "Importar";
+            this.btnImportar.UseVisualStyleBackColor = true;
+            this.btnImportar.Click += new System.EventHandler(this.btnImportar_Click);
+            // 
+            // btnExportar
+            // 
+            this.btnExportar.Location = new System.Drawing.Point(313, 52);
+            this.btnExportar.Name = "btnExportar";
+            this.btnExportar.Size = new System.Drawing.Size(95, 34);
+            this.btnExportar.TabIndex = 1;
+            this.btnExportar.Text = "Exportar";
+            this.btnExportar.UseVisualStyleBackColor = true;
+            this.btnExportar.Click += new System.EventHandler(this.btnExportar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(7, 105);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(480, 245);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 375);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnExportar;
+        private System.Windows.Forms.Button btnImportar;
+    }
+}
+
---
+++ ImportaExportaEXCEL/Form1.cs
@@ -0,0 +1,137 @@
+﻿using ExcelDataReader;
+using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Librerias usadas para el codigo
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Excel;
+using ExcelDataReader;
+using System.Data;
+
+
+namespace ImportaExportaEXCEL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImportar_Click(object sender, EventArgs e)
+        {
+            //Selecciona un archivo de excel y se establece un filtro para mostrar solo archivos .xlsx
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Archivos de Excel|*.xlsx|Archivos de Excel 97-2003|*.xls" })
+            {
+                //Si se selecciona un archivo y se da clic en abrir(aceptar) 
+                if (ofd.ShowDialog() == DialogResult.OK)
+
+                {
+                    //se abrira el archivo en modo lectura
+                    using (var stream = File.Open(ofd.FileName, FileMode.Open, FileAccess.Read))
+
+                    {
+                        //Se crea un objeto IExcel DR para leer el archivo de excel
+                        using (IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream))
+
+                        {
+                            //Se carga el contenido del archivo de excel
+                            var dataSet = reader.AsDataSet(new ExcelDataSetConfiguration()
+                            {
+                                //Configuracion del DataTable
+                                ConfigureDataTable = (tableReader) => new ExcelDataTableConfiguration()
+
+                                //True para indicar que la primera fila contiene los encabezados
+                                {
+                                    UseHeaderRow = true
+
+                                }
+                            });
+
+                            //Fuente de datos para el GridView del formulario
+                            dataGridView1.DataSource = dataSet.Tables[0];
+                        }
+
+                    }
+
+                    //Bloque para establecer los encabezados del GridView - El archivo de Excel debe tener estas cabeceras
+                    dataGridView1.Columns[0].HeaderText = "Id";
+                    dataGridView1.Columns[1].HeaderText = "Name";
+                    dataGridView1.Columns[2].HeaderText = "CellPhone";
+                    dataGridView1.Columns[3].HeaderText = "Address";
+
+                    //Ajustan el tama~o de las columnas del GridView
+                    dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                    dataGridView1.AutoResizeColumns();
+
+                    //Estilo y fuente del GridView
+                    dataGridView1.DefaultCellStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11);
+
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void btnExportar_Click(object sender, EventArgs e)
+        {
+            ExportToExcel(dataGridView1);
+        }
+
+        //Codigo generado para integrar con btnExportar_Click, metodo donde un DataGridView alimenta a un excel 
+        private void ExportToExcel(DataGridView dataGridView)
+
+        {
+            //Libreria usada *using Excel = Microsoft.Office.Interop.Excel;*
+            //Instancia de un excel
+            Excel.Application excelApp = new Excel.Application();
+            excelApp.Visible = true;
+
+            //Crear un nuevo libro
+            Workbook workbook = excelApp.Workbooks.Add();
+
+            //Seleccionar la primera hoja
+            Worksheet worksheet = (Worksheet)workbook.Sheets[1];
+
+            //Obtener el numero de columnas del DataGridView
+            int columnCount = dataGridView.ColumnCount;
+
+            //copia los valores del DataGridView al excel
+            for (int i = 0; i < columnCount; i++)
+            {
+                worksheet.Cells[1, i + 1] = dataGridView.Columns[i].HeaderText;
+            }
+
+            //Obtener el numero de filas del DataGridView
+            int rowCount = dataGridView.RowCount;
+
+            //copia los valores del DataGridView al excel
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int j = 0; j < columnCount; j++)
+                {
+                    worksheet.Cells[i + 2, j + 1] = dataGridView.Rows[i].Cells[j].Value;
+                }
+            }
+
+            //mantenerlo en memoria para luego ser salvado
+            string tempFile = System.IO.Path.GetTempFileName() + ".xls";
+
+            //Guardar el archivo que esta en memoria
+            workbook.SaveAs(tempFile);
+
+        }
+
+    }
+}
