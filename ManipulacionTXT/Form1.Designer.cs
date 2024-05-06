72330be8eade61e71448b5790b01edc4b0f37581
Alexx mzz <dani315788@gmail.com>
2024-05-06 10:11:21 -0500

    Uso de text rich el cual permite ver y modificar elementos en un text, con botones. Ver el ejemplo

---
+++ ManipulacionTXT/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace ManipulacionTXT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEjecutar = new System.Windows.Forms.Button();
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.lblDireccion = new System.Windows.Forms.Label();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.rtxtRich = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnEjecutar
+            // 
+            this.btnEjecutar.Location = new System.Drawing.Point(512, 245);
+            this.btnEjecutar.Name = "btnEjecutar";
+            this.btnEjecutar.Size = new System.Drawing.Size(129, 65);
+            this.btnEjecutar.TabIndex = 0;
+            this.btnEjecutar.Text = "Ejecutar";
+            this.btnEjecutar.UseVisualStyleBackColor = true;
+            this.btnEjecutar.Click += new System.EventHandler(this.btnEjecutar_Click);
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(512, 346);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(129, 55);
+            this.btnLeer.TabIndex = 1;
+            this.btnLeer.Text = "Leer";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(512, 128);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(129, 55);
+            this.btnGuardar.TabIndex = 3;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(512, 27);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(129, 65);
+            this.btnAbrir.TabIndex = 2;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // lblDireccion
+            // 
+            this.lblDireccion.AutoSize = true;
+            this.lblDireccion.Location = new System.Drawing.Point(25, 38);
+            this.lblDireccion.Name = "lblDireccion";
+            this.lblDireccion.Size = new System.Drawing.Size(52, 13);
+            this.lblDireccion.TabIndex = 4;
+            this.lblDireccion.Text = "Direccion";
+            this.lblDireccion.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(28, 72);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(419, 20);
+            this.txtDireccion.TabIndex = 5;
+            // 
+            // rtxtRich
+            // 
+            this.rtxtRich.Location = new System.Drawing.Point(28, 128);
+            this.rtxtRich.Name = "rtxtRich";
+            this.rtxtRich.Size = new System.Drawing.Size(419, 273);
+            this.rtxtRich.TabIndex = 6;
+            this.rtxtRich.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 435);
+            this.Controls.Add(this.rtxtRich);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.lblDireccion);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.btnLeer);
+            this.Controls.Add(this.btnEjecutar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEjecutar;
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.Label lblDireccion;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.RichTextBox rtxtRich;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ ManipulacionTXT/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ManipulacionTXT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEjecutar_Click(object sender, EventArgs e)
+        {
+            // usar la libreria using System.IO;
+            // Crea un archivo en la raiz del aplicativo \bin\Debug
+            TextWriter Escribe = new StreamWriter("Test.txt");
+
+            // Escribe lo que esta dentro del archivo y cierra el Stream
+            Escribe.WriteLine("Hola Mundo");
+            Escribe.Close();
+
+            // Crea una nueva linea en el txt \bin\Debug
+            StreamWriter agregar = File.AppendText("Test.txt");
+            agregar.Write("Nueva Linea");
+            agregar.Close();
+
+
+            MessageBox.Show("Creado");
+
+
+
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            //objeto para iniciar la lectura
+            TextReader Leer = new StreamReader("Test.txt");
+
+            //comando paraa leer la primera linea de un texto
+            //MessageBox.Show(Leer.ReadLine());
+
+            //Comando para leer la totaalidad de un texto
+            MessageBox.Show(Leer.ReadToEnd());
+            Leer.Close();
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //ups
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //programar el open file dialog
+                //Esto es una ventana de abrir
+                openFileDialog1.Title = "Busca tu archivo por favor";
+                openFileDialog1.ShowDialog();
+
+                string texto = openFileDialog1.FileName;
+
+                if (File.Exists(openFileDialog1.FileName))
+                {
+
+
+
+                    //objeto para iniciar la lectura
+                    TextReader Leer = new StreamReader(texto);
+
+                    //Comando para leer la totaalidad de un texto y asignarto al rich text
+                    rtxtRich.Text = Leer.ReadToEnd();
+
+                    Leer.Close();
+                }
+
+                //en el txtDireccion coloca la ruta del archivo abierto
+                txtDireccion.Text = texto;
+
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show("Error al abrir: " + ex.Message);
+            }
+            finally
+            {
+
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    if (File.Exists(saveFileDialog1.FileName))
+                    {
+                        string txtN = saveFileDialog1.FileName;
+
+                        StreamWriter textoPorGuardar = File.CreateText(txtN);
+
+                        textoPorGuardar.Write(rtxtRich.Text);
+                        //libera memoriaa
+                        textoPorGuardar.Flush();
+                        //cierra documento
+                        textoPorGuardar.Close();
+
+                        txtDireccion.Text = txtN;
+
+                    }
+                    else
+                    {
+                        string txtN = saveFileDialog1.FileName;
+
+                        StreamWriter textoPorGuardar = File.CreateText(txtN);
+
+                        textoPorGuardar.Write(rtxtRich.Text);
+                        //libera memoriaa
+                        textoPorGuardar.Flush();
+                        //cierra documento
+                        textoPorGuardar.Close();
+
+                        txtDireccion.Text = txtN;
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al abrir: " + ex.Message);
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
