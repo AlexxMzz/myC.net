67ee9268bc9454da2bb443224e9970335fbcfdb6
Alexx mzz <dani315788@gmail.com>
2024-05-06 08:33:37 -0500

    Escribir y leer los datos de un text mediante un form

---
+++ CREARyESCRIBIRunTxt/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace CREARyESCRIBIRunTxt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEjecutar = new System.Windows.Forms.Button();
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEjecutar
+            // 
+            this.btnEjecutar.Location = new System.Drawing.Point(145, 69);
+            this.btnEjecutar.Name = "btnEjecutar";
+            this.btnEjecutar.Size = new System.Drawing.Size(129, 65);
+            this.btnEjecutar.TabIndex = 0;
+            this.btnEjecutar.Text = "Ejecutar";
+            this.btnEjecutar.UseVisualStyleBackColor = true;
+            this.btnEjecutar.Click += new System.EventHandler(this.btnEjecutar_Click);
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(145, 168);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(129, 55);
+            this.btnLeer.TabIndex = 1;
+            this.btnLeer.Text = "Leer";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 335);
+            this.Controls.Add(this.btnLeer);
+            this.Controls.Add(this.btnEjecutar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEjecutar;
+        private System.Windows.Forms.Button btnLeer;
+    }
+}
+
---
+++ CREARyESCRIBIRunTxt/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CREARyESCRIBIRunTxt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEjecutar_Click(object sender, EventArgs e)
+        {
+            // usar la libreria using System.IO;
+            // Crea un archivo en la raiz del aplicativo \bin\Debug
+            TextWriter Escribe = new StreamWriter("Test.txt");
+
+            // Escribe lo que esta dentro del archivo y cierra el Stream
+            Escribe.WriteLine("Hola Mundo");
+            Escribe.Close();
+
+            // Crea una nueva linea en el txt \bin\Debug
+            StreamWriter agregar = File.AppendText("Test.txt");
+            agregar.Write("Nueva Linea");
+            agregar.Close();
+
+
+            MessageBox.Show("Creado");
+
+
+
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            //objeto para iniciar la lectura
+            TextReader Leer = new StreamReader("Test.txt");
+
+            //comando paraa leer la primera linea de un texto
+            //MessageBox.Show(Leer.ReadLine());
+
+            //Comando para leer la totaalidad de un texto
+            MessageBox.Show(Leer.ReadToEnd());
+            Leer.Close();
+
+
+        }
+    }
+}
